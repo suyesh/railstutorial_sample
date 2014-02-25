bef563905b6231c6b89d90b6229614ec618eb329
suyeshb@gmail.com <suyeshb@gmail.com>
2014-02-25 00:15:31 -0500

    root2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
+
+  root :to => "staticpages#home"
   get "staticpages/home"
   get "staticpages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'staticpages#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
